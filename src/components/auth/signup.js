20548fe2dbce9aa1a3e4834354a46ec37ad0140c
kguertin <kevin.m.guertin@gmail.com>
2020-06-11 16:28:01 -0400

    rework signup page

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, TextField, Typography, Button } from '@material-ui/core';
 
-function Signup () {
+export default function Signup () {
+	const [userInfo, setUserInfo] = useState({
+		username: '',
+		email: '',
+		password: '',
+		confirmPassword: ''
+	});
+
+	const handleSignUp = (e) => {
+		e.preventDefault();
+		console.log(userInfo)
+	}
+	
     return (
         <div>
             <Container>
 							<Typography variant='h2'>Enter Your Information</Typography>
-            	<form action='/signup' method='POST'>
+            	<form onSubmit={handleSignUp}>
                 <div>
-                  <TextField id="standard-basic" label="Username" />
+                  <TextField id="standard-basic" label="Username" value={userInfo.username} onChange={e => setUserInfo({...userInfo, username: e.target.value})} />
 						 		</div>   
 								 <div>
-                  <TextField id="standard-basic" label="Email" />
+                  <TextField id="standard-basic" label="Email" value={userInfo.username} onChange={e => setUserInfo({...userInfo, email: e.target.value})}/>
 						 		</div>   
 								 <div>
-                  <TextField id="standard-basic" label="Password" />
+                  <TextField id="standard-basic" label="Password" value={userInfo.username} onChange={e => setUserInfo({...userInfo, password: e.target.value})}/>
 						 		</div>   
 								 <div>
-                  <TextField id="standard-basic" label="Confirm Password" />
+									<TextField id="standard-basic" label="Confirm Password" value={userInfo.confirmPassword} onChange={e => setUserInfo({...userInfo, confirmPassword: e.target.value})} />
 						 		</div> 
 								 <Button variant='contained' type='submit'>Submit</Button>
               </form>
             </Container>
-						
-        </div>  
+        </div>
     )
 }
-
-export default Signup
