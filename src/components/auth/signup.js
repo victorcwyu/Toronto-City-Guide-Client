acca0ad63b09caa96a948b6574980bd5472dc29c
kguertin <kevin.m.guertin@gmail.com>
2020-06-11 15:21:24 -0400

    first pass over signup form

---
+++ src/components/auth/signup.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, TextField, Typography, Button } from '@material-ui/core';
+
+function Signup () {
+    return (
+        <div>
+            <Container>
+							<Typography variant='h2'>Enter Your Information</Typography>
+            	<form action='/signup' method='POST'>
+                <div>
+                  <TextField id="standard-basic" label="Username" />
+						 		</div>   
+								 <div>
+                  <TextField id="standard-basic" label="Email" />
+						 		</div>   
+								 <div>
+                  <TextField id="standard-basic" label="Password" />
+						 		</div>   
+								 <div>
+                  <TextField id="standard-basic" label="Confirm Password" />
+						 		</div> 
+								 <Button variant='contained' type='submit'>Submit</Button>
+              </form>
+            </Container>
+        </div>
+    )
+}
+
+export default Signup
