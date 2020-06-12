e15cb2a573fcaebfaf2b13a62fde5f57b603b867
neethu <msneethu2989@gmail.com>
2020-06-12 18:58:58 -0400

    Change directory structure

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container,Button } from '@material-ui/core';
-import '../styles/Schedule.scss';
+import '../../styles/Schedule.scss';
 export default function Schedule(){
   return(
     <Container className="schedule">
