764758dc808cb34a0b078ef661f42f9413df611f
Victor <victorcwyu@gmail.com>
2020-06-12 16:05:51 -0400

    Updated App.js.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 import Signup from './components/auth/signup';
 import Schedule from './components/Schedule';
-<<<<<<< HEAD
-import Login from './components/auth/login';
-=======
 import Map from './components/Map';
->>>>>>> Map component centered on Yonge-Dundas Square.
 
 function App() {
   return (
@@ -14,11 +10,7 @@ function App() {
       {/* <header className="App-header">
       </header> */}
       <Signup />
-<<<<<<< HEAD
-      <Login />
-=======
       <Map />
->>>>>>> Map component centered on Yonge-Dundas Square.
       <Schedule/>
     </div>
   );
