fb68c4b89b03a7e19b4ed8ca05f3afc4b4f9322d
Victor <victorcwyu@gmail.com>
2020-06-14 21:53:59 -0400

    Assign the Google Place Map to the autoComplete variable when the component is rendered.

--- src/components/SearchLocationInput.js
+++ src/components/SearchLocationInput.js
@@ -17,4 +17,19 @@ const loadScript = (url, callback) => {
 
   script.src = url; // load by url
   document.getElementsByTagName("head")[0].appendChild(script); // append to head
+
+  // handle when the script is loaded we will assign autoCompleteRef with google maps place autocomplete
+  function handleScriptLoad(updateQuery, autoCompleteRef) {
+    // assign autoComplete with Google maps place one time
+    autoComplete = new window.google.maps.places.Autocomplete(
+      autoCompleteRef.current,
+      { types: ["(cities)"], componentRestrictions: { country: "ca" } }
+    );
+    autoComplete.setFields(["address_components", "formatted_address"]); // specify what properties we will get from API
+    // add a listener to handle when the place is selected
+    autoComplete.addListener("place_changed", () =>
+      handlePlaceSelect(updateQuery)
+    );
+  }
+
 };
