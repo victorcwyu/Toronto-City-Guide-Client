d715a616c4987748ddf4c7370b860e3bb3f8532d
kguertin <kevin.m.guertin@gmail.com>
2020-06-15 21:06:05 -0400

    add home page that curently only displays map preview component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Map from './Map';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Map />
+        </div>
+    )
+}
