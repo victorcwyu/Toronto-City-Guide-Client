a601a9da1fa41280b1baca0cedb98bf73a466f8d
neethu <msneethu2989@gmail.com>
2020-06-20 21:38:49 -0400

    Refactor code

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Button } from "@material-ui/core";
 import "../../styles/Schedule.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ScheduleDetails from "./ScheduleDetails";
 export default function Schedule() {
   return (
