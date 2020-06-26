5e5bf2abb7aa384a3523c4d309a3c6f2bc8147b7
Victor <victorcwyu@gmail.com>
2020-06-26 14:20:38 -0400

    Toggle works as expected.

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Autocomplete from './Autocomplete';
 import "../../styles/Map.scss";
 
 import FavouritesMap from './FavouritesMap';
 
-import MapToggle from './MapToggle';
+// import MapToggle from './MapToggle';
+import Switch from '@material-ui/core/Switch';
+import UserContext from '../../context/UserContext';
 
 const GOOGLE_MAP_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 // load google map script
@@ -21,6 +23,14 @@ const loadGoogleMapScript = (callback) => {
 
 export default function Map() {
   const [loadMap, setLoadMap] = useState(false);
+
+
+  const [favouritesOn, setFavouritesOn] = useState(false);
+
+  const handleChange = (event) => {
+    setFavouritesOn(!favouritesOn);
+  };
+
   useEffect(() => {
     loadGoogleMapScript(() => {
       setLoadMap(true);
@@ -32,11 +42,16 @@ export default function Map() {
       <h1>Map</h1>
       <div id="toggle">
         <h2>Search</h2>
-        <MapToggle />
+        <Switch
+          selection={favouritesOn}
+          onChange={handleChange}
+          inputProps={{ 'aria-label': 'primary checkbox' }}
+        />
         <h2>Favourites</h2>
       </div>
-      {!loadMap ? <div>Loading...</div> : <Autocomplete />}
-      {!loadMap ? <div>Loading...</div> : <FavouritesMap />}
+      {!loadMap && <div>Loading...</div> }
+      {!favouritesOn && <Autocomplete />}
+      {favouritesOn && <FavouritesMap />}
     </div>
   )
 }
