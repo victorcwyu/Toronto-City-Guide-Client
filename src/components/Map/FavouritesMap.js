35ff37a91e8d1e31e67e4049cc6c39327791efa8
Victor <victorcwyu@gmail.com>
2020-06-25 20:55:32 -0400

    Added infowindows for markers.

--- src/components/Map/FavouritesMap.js
+++ src/components/Map/FavouritesMap.js
@@ -29,12 +29,13 @@ const FavouritesMap = () => {
     const favourites = userData.user.favourites;
 
     const favouritesCoordinates = favourites.map((favourite) => {
-      return { lat: favourite.geometry.location.lat, lng: favourite.geometry.location.lng }
+      return [
+        { lat: favourite.geometry.location.lat, lng: favourite.geometry.location.lng },
+        favourite.name, 
+        favourite.vicinity
+      ];
     })
     
-    // console.log("lat: ", favourites[0].geometry.location.lat, "lng: ", favourites[0].geometry.location.lng)
-    console.log("PLZ", favouritesCoordinates);
-
     // Initialize the Google map
     const map = new window.google.maps.Map(googleMapRef.current, {
       center: { lat: 43.6560811, lng: -79.3823601 },
@@ -42,118 +43,18 @@ const FavouritesMap = () => {
       disableDefaultUI: true
     });
 
-    // Initialize central marker
-    new window.google.maps.Marker({
-      position: map.center,
-      map: map
-    });
-
     // map through favouritesCoordinates array and add marker for each place
+    // open an infowindow when the marker is clicked
     favouritesCoordinates.forEach((place) => {
-      new window.google.maps.Marker({ position: place, map: map });
-      console.log("HI", place)
-    })
-
-    // Initialize infoWindow
-    const infoWindow = new window.google.maps.InfoWindow({
-      content: document.getElementById('info-content')
+      let marker = new window.google.maps.Marker({ position: place[0], map: map });
+      let infowindow = new window.google.maps.InfoWindow({
+        content: `<b>${place[1]}</b><br>${place[2]}`
+      });
+      marker.addListener('click', function () {
+        infowindow.open(map, marker);
+      });
     });
 
-    // initialize Google Places
-    // const places = new window.google.maps.places.PlacesService(map);
-    window.places = new window.google.maps.places.PlacesService(map);
-
-    // Search for a place type near the selected location, within the viewport of the map.
-    function search() {
-      const searchPlaceType = localStorage.getItem('searchPlaceType');
-
-      var search = {
-        bounds: map.getBounds(),
-        types: [`${searchPlaceType}`],
-      };
-      // Clear results and markers when a new search is made
-      // Ensures the results table is blank if no places are found
-      clearResults();
-      clearMarkers();
-      window.places.nearbySearch(search, function (results, status) {
-        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-          // Create a marker for each place type found, and
-          // assign a letter of the alphabetic to each marker icon.
-          for (var i = 0; i < results.length; i++) {
-            var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-            var markerIcon = MARKER_PATH + markerLetter + '.png';
-            // Use marker animation to drop the icons incrementally on the map.
-            markers[i] = new window.google.maps.Marker({
-              position: results[i].geometry.location,
-              animation: window.google.maps.Animation.DROP,
-              icon: markerIcon
-            });
-            // If the user clicks a hotel marker, show the details of that hotel
-            // in an info window.
-            markers[i].placeResult = results[i];
-            window.google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-            setTimeout(dropMarker(i), i * 100);
-            addResult(results[i], i);
-          }
-        }
-      });
-    }
-    
-    function clearMarkers() {
-      for (var i = 0; i < markers.length; i++) {
-        if (markers[i]) {
-          markers[i].setMap(null);
-        }
-      }
-      markers = [];
-    }
-    function dropMarker(i) {
-      return function () {
-        markers[i].setMap(map);
-      };
-    }
-    function addResult(result, i) {
-      var results = document.getElementById('results');
-      var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-      var markerIcon = MARKER_PATH + markerLetter + '.png';
-      var tr = document.createElement('tr');
-      tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
-      tr.onclick = function () {
-        window.google.maps.event.trigger(markers[i], 'click');
-      };
-      var iconTd = document.createElement('td');
-      var nameTd = document.createElement('td');
-      var icon = document.createElement('img');
-      icon.src = markerIcon;
-      icon.setAttribute('class', 'placeIcon');
-      icon.setAttribute('className', 'placeIcon');
-      var name = document.createTextNode(result.name);
-      iconTd.appendChild(icon);
-      nameTd.appendChild(name);
-      tr.appendChild(iconTd);
-      tr.appendChild(nameTd);
-      results.appendChild(tr);
-    }
-    function clearResults() {
-      var results = document.getElementById('results');
-      while (results.childNodes[0]) {
-        results.removeChild(results.childNodes[0]);
-      }
-    }
-    // Get the place details for a place type. Show the information in an info window,
-    // anchored on the marker for the place that the user selected.
-    function showInfoWindow() {
-      var marker = this;
-      window.uniqueId = marker.placeResult.place_id;
-      window.places.getDetails({ placeId: marker.placeResult.place_id },
-        function (place, status) {
-          if (status !== window.google.maps.places.PlacesServiceStatus.OK) {
-            return;
-          }
-          infoWindow.open(map, marker);
-          buildIWContent(place);
-        });
-    }
     // Load the place information into the HTML elements used by the info window.
     function buildIWContent(place) {
       document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
@@ -168,23 +69,7 @@ const FavouritesMap = () => {
       } else {
         document.getElementById('iw-phone-row').style.display = 'none';
       }
-      // Assign a five-star rating to the place, using a black star ('&#10029;')
-      // to indicate the rating the place has earned, and a white star ('&#10025;')
-      // for the rating points not achieved.
-      if (place.rating) {
-        var ratingHtml = '';
-        for (var i = 0; i < 5; i++) {
-          if (place.rating < (i + 0.5)) {
-            ratingHtml += '&#10025;';
-          } else {
-            ratingHtml += '&#10029;';
-          }
-          document.getElementById('iw-rating-row').style.display = '';
-          document.getElementById('iw-rating').innerHTML = ratingHtml;
-        }
-      } else {
-        document.getElementById('iw-rating-row').style.display = 'none';
-      }
+
       // The regexp isolates the first part of the URL (domain plus subdomain)
       // to give a short URL for displaying in the info window.
       if (place.website) {
@@ -200,6 +85,13 @@ const FavouritesMap = () => {
         document.getElementById('iw-website-row').style.display = 'none';
       }
     }
+
+
+
+
+
+
+
   };
 
   return (
