9fcef0915bd6b23bc8829db232539f316acdc392
Victor <victorcwyu@gmail.com>
2020-07-01 00:17:10 -0400

    Debugged initial map render.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
-import React from 'react'
-// import HomeMap from './Map/HomeMap'
-// import FavouritesHomeMap from './Map/FavouritesHomeMap'
+import React, { useContext } from 'react';
+import HomeMap from './Map/HomeMap'
 import HomeMap2 from './Map/HomeMap2'
 import CurvedText from './Map/CurvedText'
 import '../styles/CurvedText.scss'
+import UserContext from '../context/UserContext';
 
 
 export default function Home() {
+    const { userData, setUserData } = useContext(UserContext);
     return (
         <>
             <div className="wrapper">
                 <CurvedText text="where will you go today?" arc={120} radius={360} />
             </div>
             <div className="homeContainer">
-                <HomeMap2 />
+                {!userData.token && <HomeMap />}
+                {userData.token && <HomeMap2 />}
             </div>
         </>
     )
