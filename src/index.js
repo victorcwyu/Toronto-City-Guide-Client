31d5e71f7a98b68a270eaaba6115c8f65c9ffb2a
Victor <victorcwyu@gmail.com>
2020-07-06 22:03:29 -0400

    Added configuration files.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
