67147c65e990c561e93320d4150febd5aee22a1d
Victor <victorcwyu@gmail.com>
2020-09-18 17:56:19 -0400

    Deleted HomeMap2 component as it is redundant.  Replaced with FavouritesHomeMap.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,15 @@
 import React, { useContext, useState, useEffect } from 'react';
 import HomeMap from './Map/HomeMap'
-import HomeMap2 from './Map/HomeMap2'
+import FavouritesHomeMap from './Map/FavouritesHomeMap';
 import CurvedText from './Map/CurvedText'
 import '../styles/Home.scss'
 import UserContext from '../context/UserContext';
-
 
 export default function Home() {
     const { userData } = useContext(UserContext);
 
     const [windowWidth, setWindowWidth] = useState('');
-    
+
     useEffect(() => {
         setWindowWidth(window.innerWidth);
         window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
@@ -28,17 +27,17 @@ export default function Home() {
                     }
                     {(windowWidth < 500 && windowWidth >= 451) &&
                         <CurvedText text="where will you go today?" arc={80} radius={360} />
-                    }   
+                    }
                     {(windowWidth < 451 && windowWidth >= 375) &&
                         <CurvedText text="where will you go today?" arc={60} radius={360} />
-                    }   
+                    }
                     {(windowWidth < 375) &&
                         <CurvedText text="where will you go today?" arc={50} radius={360} />
-                    }   
+                    }
                 </div>
                 <div className="homeMapContainer">
                     {!userData.token && <HomeMap />}
-                    {userData.token && <HomeMap2 />}
+                    {userData.token && <FavouritesHomeMap />}
                 </div>
             </div>
         </>
