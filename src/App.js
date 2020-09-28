4b1eba667951e79e28d33e3dac3c6b90051fe082
Victor <victorcwyu@gmail.com>
2020-09-28 17:41:15 -0400

    Edited App.js to debug redirects.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
-import Signup from './components/auth/Signup';
-import Login from './components/auth/Login';
-import Home from './components/Home';
-import Header from './components/Layout/Header';
-import UserContext from './context/UserContext';
-import UserProfile from './components/UserProfile'
-import Axios from 'axios';
-import Map from './components/Map/Map';
-import index from './components/Schedule/index';
-import Messages from './components/Messages';
-import FAQ from './components/FAQ';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Signup from "./components/auth/Signup";
+import Login from "./components/auth/Login";
+import Home from "./components/Home";
+import Header from "./components/Layout/Header";
+import UserContext from "./context/UserContext";
+import UserProfile from "./components/UserProfile";
+import Axios from "axios";
+import Map from "./components/Map/Map";
+import index from "./components/Schedule/index";
+import Messages from "./components/Messages";
+import FAQ from "./components/FAQ";
 
 function App() {
-
   const [userData, setUserData] = useState({
     token: undefined,
     user: undefined,
-    wtf: 0
+    wtf: 0,
   });
 
   useEffect(() => {
-
     const checkLoggedIn = async () => {
-      let token = localStorage.getItem('auth-token');
+      let token = localStorage.getItem("auth-token");
       if (token === null) {
-        localStorage.setItem('auth-token', '');
-        token = '';
+        localStorage.setItem("auth-token", "");
+        token = "";
       }
-      const tokenRes = await Axios.post('https://toronto-city-travel-guide.herokuapp.com/auth/isTokenValid', null, {
-        headers: {
-          "x-auth-token": token
+      const tokenRes = await Axios.post(
+        "https://toronto-city-travel-guide.herokuapp.com/auth/isTokenValid",
+        null,
+        {
+          headers: {
+            "x-auth-token": token,
+          },
         }
-      });
+      );
 
       if (tokenRes.data) {
-        const userRes = await Axios.post('https://toronto-city-travel-guide.herokuapp.com/getActiveUser', null, {
-          headers: {
-            "x-auth-token": token
+        const userRes = await Axios.post(
+          "https://toronto-city-travel-guide.herokuapp.com/getActiveUser",
+          null,
+          {
+            headers: {
+              "x-auth-token": token,
+            },
           }
-        });
+        );
 
         setUserData({
           token: token,
-          user: userRes.data
-        })
+          user: userRes.data,
+        });
       }
-    }
-    checkLoggedIn()
+    };
+    checkLoggedIn();
   }, [userData.token]);
 
   return (
@@ -58,14 +64,14 @@ function App() {
           {/* Added temp header to handle nav to cut down on clutter we can add a proper styled nav */}
           <Header />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path='/signup' component={Signup} />
-            <Route path='/map' component={Map} />
-            <Route path='/userProfile' component={UserProfile} />
-            <Route exact path='/schedule' component={index} />
-            <Route path='/messages' component={Messages} />
-            <Route path='/faq' component={FAQ} />
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/signup" exact component={Signup} />
+            <Route path="/map" exact component={Map} />
+            <Route path="/userProfile" exact component={UserProfile} />
+            <Route path="/schedule" exact component={index} />
+            <Route path="/messages" exact component={Messages} />
+            <Route path="/faq" exact component={FAQ} />
           </Switch>
         </UserContext.Provider>
       </BrowserRouter>
@@ -74,7 +80,6 @@ function App() {
 }
 export default App;
 
-
 // why cant we keep state on refresh
-// funtion calling itself on add favourites 
+// funtion calling itself on add favourites
 // map rerender
